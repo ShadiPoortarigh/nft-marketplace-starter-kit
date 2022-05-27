57f7998dbc8fa07d0b10a6c610bbe5190b31284e
maryam <maryam.poortarigh@gmail.com>
2022-05-27 17:41:35 +0430

    Solidity Project

---
+++ src/contracts/Greenbox.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.
+
+pragma solidity ^0.8.4;
+
+
+
+contract Greenbox {
+    string private name;
+    string private symbol;
+
+
+    constructor(){
+        name = 'boxForGreen';
+        symbol = 'XDF76';
+    }
+
+    
+}
